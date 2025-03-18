af3a8cf5f307c6a77008b1b3070b77e32d191539
Đào Thiện Tuấn - HC <tuandt1@rocketstudio.com.vn>
2025-03-18 11:55:43 +0700

    Add gravity

--- Assets/Scripts/CharacterMovementController.cs
+++ Assets/Scripts/CharacterMovementController.cs
@@ -6,16 +6,24 @@ public class CharacterMovementController : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float gravity;
     [SerializeField] private Transform fpsCameraContainer;
     [SerializeField, Range(0f, 1f)] private float lookDeltaScaler;
     [SerializeField, Range(0f, 90f)] private float maxVerticalLookRange;
 
     private float _lookPitch = 0f;
+    private float _velocityY = 0f;
 
     public void Move(Vector2 ratios)
     {
-        var movement = controller.transform.forward * ratios.y + controller.transform.right * ratios.x;
-        controller.Move(moveSpeed * Time.deltaTime * movement);
+        _velocityY += gravity * Time.deltaTime;
+        if (controller.isGrounded)
+        {
+            _velocityY = 0f;
+        }
+
+        var velocity = (controller.transform.forward * ratios.y + controller.transform.right * ratios.x) * moveSpeed + Vector3.down * _velocityY;
+        controller.Move(velocity * Time.deltaTime);
     }
 
     public void Look(Vector2 lookDelta)
